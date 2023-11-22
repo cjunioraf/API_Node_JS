804a9810d4897ff01226ae9948098c8fad78b44f
cjunioraf <clecio,ferreira@gmail.com>
2023-11-22 17:22:58 -0300

    API NODE + EXPRESS + MONGOOSEDB CONCLUIDA

--- routes/personroutes.js
+++ routes/personroutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 
+const { Router } = require('express');
 const Pessoa = require('../models/Pessoa')
 //rota inicial / enpoint  "/" é a home, página inicial de qualquer site 
 //BUSCA DE DADOS 
@@ -7,8 +8,8 @@ router.get('/', async (req, res) => {
 
     try{        
         //await - espera todos os dados do db para retornar
-        const pessoas = await Pessoa.find()
-        res.status(200).json(pessoas)
+        const $pessoa = await Pessoa.find()
+        res.status(200).json($pessoa)
     }
     catch(erro){
         res.status(500).json({error: error})
@@ -25,10 +26,10 @@ router.get('/:id', async (req, res) => {
     try{        
         //await - espera todos os dados do db para retornar
         //_id pq é o padrão do db MongoDB - Atlas
-        const pessoas = await Pessoa.findOne({_id: $id})
+        const $pessoa = await Pessoa.findOne({_id: $id})
         
-        if(pessoas != null)
-            res.status(200).json(pessoas)
+        if($pessoa != null)
+            res.status(200).json($pessoa)
         else{
             res.status(424).json({error:'Pessoa não localizada!'})
         }                
@@ -60,11 +61,11 @@ router.post('/', async (req, res) =>{
         return
     }
 
-    const pessoa = {nome, salario, aprovado, idade}
+    const $pessoa = {nome, salario, aprovado, idade}
     
     try{
         //criar o registro 
-        await Pessoa.create(pessoa) 
+        await Pessoa.create($pessoa) 
         //status HTTP - status de sucesso na requisição POST
         res.status(201).json({msg: 'Inserido com sucesso!'})
 
@@ -75,6 +76,45 @@ router.post('/', async (req, res) =>{
 })
 
 //UPDATE NO REGISTRO (PUT E PATCH) - PUT É O OBJETO COMPLETO - PATCH É DADOS PARCIAIS PARA SEREM ATUALIZADOS.
+router.patch('/:id', async (req, res) => {
+
+    const $id = req.params.id
+    const {nome, salario, aprovado, idade} = req.body
+    const $pessoa = {nome, salario, aprovado, idade}
+
+    try{                
+        const $updatepessoa = await Pessoa.updateOne({_id: $id}, $pessoa) 
+        //valida quantos registros foram atualizados no db
+        if($updatepessoa.matchedCount === 0){
+            res.status(422).json({error:'Pessoa não localizada.'})
+            return
+        }
+
+        res.status(200).json($pessoa)
+    }
+    catch(erro){
+        res.status(500).json({error: error})
+    }
+})
+//DELETAR DADOS
+router.delete('/:id', async (req, res) => {
+    
+    const $id = req.params.id
+    const $pessoa = await Pessoa.findOne({_id: $id})
+
+    if(!$pessoa){
+        res.status(424).json({error:'Pessoa não localizada!'})
+        return 
+    }        
+
+    try{        
+        await Pessoa.deleteOne({_id: $id}) 
+        res.status(200).json({msg: 'Pessoa removida com sucesso'})
+    }
+    catch(erro){
+        res.status(500).json({error: error})
+    }
+})
 
 
 module.exports = router
